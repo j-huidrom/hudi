1384b78badf2e570287608da27bdf5e6de87268c
Jashyawanta Huidrom <hellohudi@gmail.com>
2026-07-14 21:20:54 +0530

    fix import issue

--- app/static/facecore/main.js
+++ app/static/facecore/main.js
@@ -1,24 +1,5 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164/build/three.module.js";
-
 import { createScene } from "./scene.js";
 import { startRenderer } from "./renderer.js";
 
 createScene();
 startRenderer();
-
-const clock = new THREE.Clock();
-
-function animate() {
-
-    requestAnimationFrame(animate);
-
-    renderer.render(
-
-        scene,
-        camera
-
-    );
-
-}
-
-animate();
