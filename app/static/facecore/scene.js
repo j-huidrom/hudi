37693a73c1074ef57a9b5670cc8ffb63d31c7ad7
Jashyawanta Huidrom <hellohudi@gmail.com>
2026-07-14 19:00:47 +0530

    add particles in scene

--- app/static/facecore/scene.js
+++ app/static/facecore/scene.js
@@ -1,6 +1,7 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164/build/three.module.js";
 import { createOrb } from "./orb.js";
 import { createCore } from "./core.js";
+import { createParticles } from "./particles.js";
 
 export let scene;
 export let camera;
@@ -57,5 +58,7 @@ export function createScene() {
     createOrb(scene);
 
     createCore(scene);
+
+    createParticles(scene);
 
 }
